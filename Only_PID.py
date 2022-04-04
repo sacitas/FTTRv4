a0934baaf59494ad654b2dc8a3e2b0dec0e71a39
sacitas <100199653+sacitas@users.noreply.github.com>
2022-04-04 17:35:27 +0200

    Update Only_PID.py

--- Only_PID.py
+++ Only_PID.py
@@ -84,61 +84,50 @@ class PID():
   
     def Compute(self, PV):
         if self.stop == False:
-            while True:
-                #Error term
-                self.e = self.SP - PV
+            #Error term
+            self.e = self.SP - PV
     
-                #Proportional term
-                self.P = self.Kp * self.e
+            #Proportional term
+            self.P = self.Kp * self.e
     
-                #Integral term
-                if self.Ti == 0:
-                     self.Ki = 0
-                     self.I = 0
-                else:
-                    self.Ki = self.Kp / self.Ti
-                    self.I += self.Ki * self.e * self.dt
+            #Integral term
+            if self.Ti == 0:
+                 self.Ki = 0
+                 self.I = 0
+            else:
+                self.Ki = self.Kp / self.Ti
+                self.I += self.Ki * self.e * self.dt
     
-                #Saturation Integral term
-                if self.I >= self.max_windup:
-                    self.I = self.max_windup
-                elif self.I <= self.min_windup:
-                    self.I = self.min_windup
+            #Saturation Integral term
+            if self.I >= self.max_windup:
+                self.I = self.max_windup
+            elif self.I <= self.min_windup:
+                self.I = self.min_windup
     
-                #Derivative term and Beta
-                if (self.Td + self.dt * self.N) > 0:
-                    self.Beta = self.Td/(self.Td + self.dt * self.N)
-                else:
-                    self.Beta = 0
+            #Derivative term and Beta
+            if (self.Td + self.dt * self.N) > 0:
+                self.Beta = self.Td/(self.Td + self.dt * self.N)
+            else:
+                self.Beta = 0
     
-                self.D = self.Beta * self.D - self.Kd/dt * (1 - self.Beta)*(PV - self.PV_prev)
+            self.D = self.Beta * self.D - self.Kd/dt * (1 - self.Beta)*(PV - self.PV_prev)
     
-                #update stored data for next calculation
-                self.PV_prev = PV
+            #update stored data for next calculation
+            self.PV_prev = PV
     
-                #Computed value
-                self.output = self.P + self.I + self.D
+            #Computed value
+            self.output = self.P + self.I + self.D
     
-                #Saturation output 
-                if self.output >= self.max_output:
-                    self.output = self.max_output
-                elif self.output <= self.min_output:
-                    self.output = self.min_output
-                
+            #Saturation output 
+            if self.output >= self.max_output:
+                self.output = self.max_output
+            elif self.output <= self.min_output:
+                self.output = self.min_output
             
-                try: 
-                    pwm.ChangeDutyCycle(self.output) 
-                except KeyboardInterrupt:
-                    pwm.stop()
-                    GPIO.cleanup() # cleanup all GPIO 
+            return self.output
     
         elif self.stop == True:
             pass
-        
-    def run(self):
-        #Thread the function over to let it run in the background
-        threading.Thread(target = self.Compute,  args=(PV,)).start()   
-   
     
     def setstop(self, stop):
         self.stop = stop
@@ -163,5 +152,17 @@ class PID():
         
 #Call the class to start the PID controller            
 PID = PID(SP, Kp, Ti, Td, N, dt)
-PID.run()
+
+
+
+def main():   
+    while True:              
+        try: 
+            pwm.ChangeDutyCycle(PID.Compute(PV)) 
+        except KeyboardInterrupt:
+            pwm.stop()
+            GPIO.cleanup() # cleanup all GPIO 
+    
+#Thread the function over to let it run in the background
+threading.Thread(target = main).start()   
               
