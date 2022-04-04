38a48a490673b39babb38199522a6ae620e21100
sacitas <100199653+sacitas@users.noreply.github.com>
2022-04-04 17:54:31 +0200

    Update PID_FTTR_V4_FINAL.py

--- PID_FTTR_V4_FINAL.py
+++ PID_FTTR_V4_FINAL.py
@@ -1,175 +1,166 @@
+#PID Controller
+
+#Import necessary packages
 import RPi.GPIO as GPIO
 import time
-import os.path
+import threading
 
-# Parameters
-Ts = float(input("Enter a samplingtime: "))
-SP = float(input("Enter a setpoint: "))
-K_p = float(input("Enter a K_p value: "))
-T_i = float(input("Enter a T_i value: "))
-T_d = float(input("Enter a T_d value: "))
-N = float(input("Enter a filter (N) value: "))
-ManVal = 0
-T_t = 0
-Tr_gain = 0
-U_total = 0
-PV = [0,0]
-e = [0, 0]
-U_i = [0, 0]
-U_d = [0, 0]
+#Definition of variables
+#SP      - Setpoint 
+#PV      - Process value
+#Kp      - Proportional gain
+#Ki      - Integral gain
+#Kd      - Derivative gain
+#Td      - Derivative time 
+#N       - Filter coefficient
+#dt      - Sampling time
+#Beta    - Beta?
+#e       - error value
+#PV_prev - Process value value previous
+
+#### Startup parameters ####
+SP = 100 #Setpoint
+Kp = 1   #Proportional gain
+Ti = 0   #Integral time
+Td = 0   #Derivative time  
+N = 10   #filter coefficient
+dt = 5  #Sampling time
+PV = 0   #Process value readings
+
 
 PWM_pin = 33 # PWM pin on Raspberry Pi
- 
-# Setup of the PWM pin on the Raspberry Pi
-def setup():
-    global pwm
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(PWM_pin, GPIO.OUT)
-    GPIO.output(PWM_pin, GPIO.LOW)
-    pwm = GPIO.PWM(PWM_pin, 1000) # Set Frequency to 1 KHz
-    pwm.start(0) # Set the starting Duty Cycle
+
+
+class PID():
+    def __init__(self, SP, Kp, Ti, Td, N, dt, PWM_pin): 
         
-# Destroy PWM pin
-def destroy():
-    pwm.stop()
-    GPIO.output(PWM_pin, GPIO.LOW)
-    GPIO.cleanup()
-    
-def createConfig():
-    file_exists = os.path.exists('pid.conf')
-    if file_exists == True:
-        os.remove("pid.conf")
-    else:
-        pass
-    with open ('pid.conf', 'w') as f:
-        f.write("#######################\n")
-        f.write("PID-controller settings\n")
-        f.write("#######################\n\n")
-        f.write('SP: %s\nK_p: %s\nT_i: %s\nT_d: %s\nAuto (0 or 1): %s\nManual value (0 - 100): %s'%(SP,K_p,T_i,T_d,Auto,ManVal))
+        self.PWM_pin = PWM_pin
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.PWM_pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.PWM_pin, 1000) # Set Frequency to 1 KHz
+        self.pwm.start(0) # Set the starting Duty Cycle
+        
+        
+        threading.Thread(target = self.Compute, args=(PV,)).start()
             
-def readConfig():
-    global SP, K_p, T_i, T_d, Auto, ManVal
-    with open ('pid.conf', 'r+') as f:
-        for i in range(4):
-            f.readline()
-        SP_read = f.readline().split(':')
-        SP = float(SP_read[1])
-        K_p_read = f.readline().split(':')
-        K_p = float(K_p_read[1])
-        T_i_read = f.readline().split(':')
-        T_i = float(T_i_read[1])
-        T_d_read = f.readline().split(':')
-        T_d = float(T_d_read[1])
-        Auto_read = f.readline().split(':')
-        Auto = int(Auto_read[1])
-        ManVal_read = f.readline().split(':')
-        ManVal = int(ManVal_read[1])
+        #Setpoint
+        self.SP = SP
+    
+        #Gain parameters for proportional, integral and derivative 
+        self.Kp = Kp
         
-# PID-controller
-def FTTR_PID(Ts, SP, PV, K_p, T_i, T_d, T_t, Tr_gain, U_total):
-    # Check for zero division
-    if(T_i > 0):
-        alpha = Ts/T_i
-    else:
-        alpha = 0
+        self.Ti = Ti
+        if self.Ti == 0:
+            self.Ki = 0
+        else:
+            self.Ki = self.Kp / self.Ti
+          
+        self.Td = Td
+        self.Kd = self.Kp * self.Td
+    
+        #Filter coefficient
+        self.N = N
         
-    if(T_t > 0):
-        gamma = Ts/T_t
-    else:
-        gamma = 0
+        #Sampling time
+        self.dt = dt
         
-    if((T_d + Ts*N)>0):
-        beta = T_d/(T_d+Ts*N)
-    else:
-        beta = 0
+        #Beta
+        if (self.Td + self.dt * self.N) > 0:
+            self.Beta = self.Td/(self.Td + self.dt * self.N)
+        else:
+            self.Beta = 0
+                    
+        #Limits for saturation and output
+        self.max_windup = 100
+        self.min_windup = 0
+        self.max_output = 100
+        self.min_output = 0
     
-    # Calculate error from setpoint
-    e[0] = SP - PV[0]
-    
-    # Proportional control
-    U_p = K_p * e[0]
-    
-    # Clamp P-term
-    if(U_p>100):
-        U_p = 100
-    elif(U_p<0):
-        U_p = 0
-    
-    # Integral control with anti-windup (back calculation)
-    if T_i == 0:
-        U_i[0] = 0
-    else:
-        U_i[0] = U_i[1] + (K_p * alpha * e[0]) + gamma*(Tr_gain - U_total)
-    
-    # Clamp I-term
-    if(U_i[0]>100):
-        U_i[0] = 100
-    elif(U_i[0]<0):
-        U_i[0] = 0
+        #Initial condition 
+        self.e = 0
+        self.e_prev = 0
+        self.PV_prev = 0
+        self.P = 0
+        self.I = 0
+        self.D = 0
         
-    # Derivative control 
-    U_d[0] = beta*U_d[1] - K_p*(T_d/Ts)*(1-beta)*(PV[0]-PV[1])
+        #Startup flag to stop/pause or continue the controller
+        self.stop = False
+  
+    def Compute(self, PV):
+        if self.stop == False:
+                while True:
+                    #Error term
+                    self.e = self.SP - PV
+            
+                    #Proportional term
+                    self.P = self.Kp * self.e
+            
+                    #Integral term
+                    if self.Ti == 0:
+                         self.Ki = 0
+                         self.I = 0
+                    else:
+                        self.Ki = self.Kp / self.Ti
+                        self.I += self.Ki * self.e * self.dt
+            
+                    #Saturation Integral term
+                    if self.I >= self.max_windup:
+                        self.I = self.max_windup
+                    elif self.I <= self.min_windup:
+                        self.I = self.min_windup
+            
+                    #Derivative term and Beta
+                    if (self.Td + self.dt * self.N) > 0:
+                        self.Beta = self.Td/(self.Td + self.dt * self.N)
+                    else:
+                        self.Beta = 0
+            
+                    self.D = self.Beta * self.D - self.Kd/dt * (1 - self.Beta)*(PV - self.PV_prev)
+            
+                    #update stored data for next calculation
+                    self.PV_prev = PV
+            
+                    #Computed value
+                    output = self.P + self.I + self.D
+            
+                    #Saturation output 
+                    if self.output >= self.max_output:
+                        self.output = self.max_output
+                    elif self.output <= self.min_output:
+                        self.output = self.min_output
+                    
+                    try:
+                        self.pwm.ChangeDutyCycle(output)
+                    except KeyboardInterrupt:
+                        self.pwm.stop()
+                        GPIO.cleanup() # cleanup all GPIO 
+        
+        elif self.stop == True:
+            pass
     
-    # Clamp D-term
-    if(U_d[0]>100):
-        U_d[0] = 100
-    elif(U_d[0]<0):
-        U_d[0] = 0
-    
-    # Total control
-    U_total = U_p + U_i[0] + U_d[0]
-    
-    # Clamp total control
-    if(U_total>100):
-        U_total = 100
-    elif(U_total<0):
-        U_total = 0
-    
-    # Update values
-    e[1] = e[0]
-    PV[1] = PV[0]
-    U_i[1] = U_i[0]
-    U_d[1] = U_d[0]
-    
-    print(U_total)
-    
-    pwm.ChangeDutyCycle(U_total)
+    def setstop(self, stop):
+        self.stop = stop
 
-    # Samplingtime 
-    time.sleep(Ts)
-    
-    return U_total
-    
-def PID_loop():
-    readConfig()
-    FTTR_PID(Ts, SP, PV, K_p, T_i, T_d, T_t, Tr_gain, U_total)
-    
-def ManVal_loop():
-    readConfig()
-    man_output = ManVal
-    print(man_output)
-    pwm.ChangeDutyCycle(man_output)
-    time.sleep(Ts)
+    def setSP(self, Setpoint):
+        self.SP = Setpoint
         
-# default mode    
-Auto = 0
-
-# User input
-user_input = int(input("Enter Auto (1) or Manual (0): "))
-
-if user_input == 1:
-    Auto = 1
-else:
-    Auto = 0
-    
-if  __name__ == '__main__':
-    createConfig()
-    setup()
-    try:
-        while True:
-            if Auto == 1:
-                PID_loop()
-            elif Auto == 0:
-                ManVal_loop()
-    except KeyboardInterrupt:
-        destroy()
+    def setKp(self, Kp):
+        self.Kp = Kp
+        
+    def setTi(self, Ti):
+        self.Ti = Ti
+ 
+    def setTd(self, Td):
+        self.Td = Td
+        
+    def setN(self, N):
+        self.N = N
+       
+    def setdt(self, dt):
+        self.dt = dt
+        
+        
+#Call the class to start the PID controller            
+PID = PID(SP, Kp, Ti, Td, N, dt, PWM_pin)
