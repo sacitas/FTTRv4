1fd7fb6717cf7d2c826773184cfd192d4e15c23f
haakondm <74726804+haakondm@users.noreply.github.com>
2022-04-05 20:50:57 +0200

    Live plotter for digital sensors

---
+++ temp_gen_digi.py
@@ -0,0 +1,79 @@
+import os
+import glob
+from time import sleep
+import csv
+import datetime as dt
+
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+
+x = dt.datetime.now().strftime('%H:%M:%S')
+
+fieldnames = ["x", "temp0", "temp1", "temp2"]
+ 
+def read_temp_raw(n):
+    device_folder = glob.glob(base_dir + '28*')[n]
+    device_file = device_folder + '/w1_slave'
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def convert_temp(n):
+    lines = read_temp_raw(n)
+
+    while lines[0].strip()[-3:] != 'YES':
+        lines = read_temp_raw(n)
+
+    equals_pos = lines[1].find('t=')
+
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+
+        if temp_c > 2048:
+            temp_c = temp_c - 4096
+
+        return temp_c
+
+
+
+def read_temp():
+    temps = []
+
+    for i in range (0, 3):
+        temp = convert_temp(i)
+        temps.append(temp)
+        print("Temp_sensor{} = {}*C.".format(i, temp))
+
+    return temps
+
+
+
+with open('test_data.csv', 'w') as data_csv:
+    csv_writer = csv.DictWriter(data_csv, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+	
+while True:	
+    temps = read_temp()
+
+    with open('test_data.csv', 'a') as data_csv:
+        csv_writer = csv.DictWriter(data_csv, fieldnames=fieldnames)
+        
+        info = {
+            "x": x,
+            "temp0": temps[0],
+            "temp1": temps[1],
+            "temp2": temps[2]
+        }
+        csv_writer.writerow(info)
+        data_csv.close()
+        
+        x = dt.datetime.now().strftime('%H:%M:%S')
+
+    sleep(0.1)
