087ba0d000ca991e0bb6d4449b436c1cf78a83d1
Martin <73833788+Embermire@users.noreply.github.com>
2022-04-06 10:53:30 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,26 +2,6 @@ import PID_FTTR_V4_FINAL as PID
 #import temp_gen_digi as temp
 
 def main():
-    # default mode    
-    Auto = 0
-
-    # User input
-    user_input = int(input("Enter Auto (1) or Manual (0): "))
-
-    if user_input == 1:
-        Auto = 1
-    else:
-        Auto = 0
-
-    PID.createConfig()
-    PID.setup()
-    try:
-        while True:
-            if Auto == 1:
-                PID.PID_loop()
-            elif Auto == 0:
-                PID.ManVal_loop()
-    except KeyboardInterrupt:
-        destroy()
+    pass
 
 main()
