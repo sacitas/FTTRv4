b4a51d24f8eb2d3ee80d37cda101d2ec091694b5
Martin <73833788+Embermire@users.noreply.github.com>
2022-04-06 11:05:48 +0200

    Update PID_test.py

--- PID_test.py
+++ PID_test.py
@@ -18,6 +18,8 @@ e = [0, 0]
 U_i = [0, 0]
 U_d = [0, 0]
 
+# default mode    
+Auto = 0
 PWM_pin = 33 # PWM pin on Raspberry Pi
  
 # Setup of the PWM pin on the Raspberry Pi
@@ -152,9 +154,6 @@ def ManVal_loop():
     time.sleep(Ts)
 
 def PID_main():
-    # default mode    
-    Auto = 0
-
     # User input
     user_input = int(input("Enter Auto (1) or Manual (0): "))
 
