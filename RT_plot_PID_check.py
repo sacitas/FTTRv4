b06af654cf42cd9a91a37c053014a7e78586cf75
haakondm <74726804+haakondm@users.noreply.github.com>
2022-04-20 09:44:48 +0200

    Update RT_plot_PID_check.py

--- RT_plot_PID_check.py
+++ RT_plot_PID_check.py
@@ -36,48 +36,18 @@ def animate(i):
 
     plt.cla()
     
-    t0, = plt.plot(x, temp0, linewidth = 1.5, label='Sensor 0')
-    t1, = plt.plot(x, temp1, linewidth = 1.5, label='Sensor 1')
-    t2, = plt.plot(x, temp2, linewidth = 1.5, label='Sensor 2')
-    t3, = plt.plot(x, temp3, linewidth = 1.5, label='Sensor 3')
-    t4, = plt.plot(x, temp4, linewidth = 1.5, label='Sensor 4')
+    plt.plot(x, temp0, linewidth = 1.5, label='Sensor 0')
+    plt.plot(x, temp1, linewidth = 1.5, label='Sensor 1')
+    plt.plot(x, temp2, linewidth = 1.5, label='Sensor 2')
+    plt.plot(x, temp3, linewidth = 1.5, label='Sensor 3')
+    plt.plot(x, temp4, linewidth = 1.5, label='Sensor 4')
     
     
     plt.xticks(rotation=90, ha='right', fontsize=12)
     plt.xticks(np.arange(0, len(x)+1, 20))
-    
-legend = plt.legend(loc='upper left')
-t0_legend, t1_legend, t2_legend, t3_legend, t4_legend = legend.get_lines()
-t0_legend.set_picker(True)
-t0_legend.set_pickradius(10)
-t1_legend.set_picker(True)
-t1_legend.set_pickradius(10)
-t2_legend.set_picker(True)
-t2_legend.set_pickradius(10)
-t3_legend.set_picker(True)
-t3_legend.set_pickradius(10)
-t4_legend.set_picker(True)
-t4_legend.set_pickradius(10)
+    plt.legend(loc='upper left')
+    plt.tight_layout()
 
-plt.tight_layout()
-
-graphs = {}
-graphs[t0_legend] = t0
-graphs[t1_legend] = t1
-graphs[t2_legend] = t2
-graphs[t3_legend] = t3
-graphs[t4_legend] = t4
-
-    
-def on_pick(event):
-    legend = event.artist
-    isVisible = legend.get_visible()
-    
-    graphs[legend].set_visible(not isVisible)
-    legend.set_visible(not isVisible)
-    
-    
-plt.connect('pick_event', on_pick)
     
 canvas = FigureCanvasTkAgg(plt.gcf(), master=root)
 canvas.get_tk_widget().place(x = 10, y = 10, width = 600, height = 400)
@@ -87,13 +57,10 @@ canvas.draw()
 ani = FuncAnimation(plt.gcf(), animate, interval=500)
 
 #------Create buttons------
-root.update()
-A = tk.Button(root, text = "Auto", font = ('calibri', 12), command = lambda: mode_auto())
-A.place(x = 670, y = 30)
 
-root.update()
-M = tk.Button(root, text = "Manual", font = ('calibri', 12), command = lambda: mode_manual())
-M.place(x = A.winfo_x()+A.winfo_reqwidth() + 10, y = 30)
+var0 = tk.IntVar()
+MA = tk.Checkbutton(root, text='Manual', variable=var0, onvalue=1, offvalue=0)
+MA.place(x = 670, y = 50)
 
 root.update()
 S_P = tk.Button(root, text = "Save plot", font = ('calibri', 12), command = lambda: save_plot())
@@ -135,27 +102,6 @@ td_ent = tk.Entry(root)
 td_ent.insert(0, "0")
 td_ent.place(x = 670, y = ti_label.winfo_y()+ti_label.winfo_reqwidth() + 10)
 
-root.update()
-auto_label = tk.Label(root, text = 'Auto:', font = ('calibre', 10))
-auto_label.place(x = 630, y = td_label.winfo_y()+td_label.winfo_reqwidth() + 10)
-auto_ent = tk.Entry(root)
-auto_ent.insert(0, "1")
-auto_ent.place(x = 670, y = td_label.winfo_y()+td_label.winfo_reqwidth() + 10)
-
-root.update()
-man_label = tk.Label(root, text = 'Manual:', font = ('calibre', 10))
-man_label.place(x = 610, y = 300)
-man_ent = tk.Entry(root)
-man_ent.insert(0, "0")
-man_ent.place(x = 670, y = 300)
-
-
-var0 = tk.IntVar()
-M0 = tk.Checkbutton(root, text='Manual', variable=var0, onvalue=1, offvalue=0, command = lambda: mode_manual())
-M0.place(x = 50, y = 420)
-
-
-
         
 def SetRegVals():
     
@@ -171,9 +117,11 @@ def SetRegVals():
     td = float(td)
     auto = auto_ent.get()
     auto = int(auto)
-    man = man_ent.get()
-    man = int(man)
     
+    if (var0.get() == 1):
+        man = 1
+    else:
+        man = 0
 
     with open ('pid.conf', 'w') as f:
         f.write('%s,%s,%s,%s,%s,%s'%(sp,kp,ti,td,auto,man))
