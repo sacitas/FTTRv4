12f7098ee7efe3636dfbb03a8049a896c4403a11
haakondm <74726804+haakondm@users.noreply.github.com>
2022-04-21 12:03:12 +0200

    Update temp_PID.py

--- temp_PID.py
+++ temp_PID.py
@@ -3,6 +3,17 @@ import glob
 from time import sleep
 import csv
 import datetime as dt
+import adafruit_ads1x15.ads1115 as ADS
+import board
+import busio
+from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ads1x15.ads1115 import Mode
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+ads = ADS.ADS1115(i2c)
+
+ads.mode = Mode.CONTINUOUS
 
 
 os.system('modprobe w1-gpio')
@@ -10,7 +21,7 @@ os.system('modprobe w1-therm')
  
 base_dir = '/sys/bus/w1/devices/'
 
-fieldnames = ["x", "temp0", "temp1", "temp2", "temp3", "temp4"]
+fieldnames = ["x", "dtemp0", "dtemp1", "dtemp2", "dtemp3", "dtemp4", "atemp0", "atemp1"]
  
 def read_temp_raw(n):
     device_folder = glob.glob(base_dir + '28*')[n]
@@ -67,6 +78,8 @@ def create_tmpFile():
 
 def write_tmp():
     x = dt.datetime.now().strftime('%H:%M:%S')
+    atemp0 = 22
+    atemp1 = 22
     temps = read_temp()
 
     with open('PID_temp.csv', 'a') as data_csv:
@@ -74,15 +87,28 @@ def write_tmp():
         
         info = {
             "x": x,
-            "temp0": temps[0],
-            "temp1": temps[1],
-            "temp2": temps[2],
-            "temp3": temps[3],
-            "temp4": temps[4]
+            "dtemp0": temps[0],
+            "dtemp1": temps[1],
+            "dtemp2": temps[2],
+            "dtemp3": temps[3],
+            "dtemp4": temps[4],
+            "atemp0": atemp0,
+            "atemp1": atemp1
         }
         csv_writer.writerow(info)
         data_csv.close()
         
         x = dt.datetime.now().strftime('%H:%M:%S')
+        
+        chan0 = AnalogIn(ads, ADS.P0)
+        chan1 = AnalogIn(ads, ADS.P1)
+        S1 = chan0.value
+        V1 = chan0.voltage
+        atemp0 = V1 / (11/1000)
+        atemp0 = float(round(atemp0, 1))
+        S2 = chan1.value
+        V2 = chan1.voltage
+        atemp0 = V2 / (11/1000)
+        atemp1 = float(round(atemp1, 1))
 
     sleep(0.1)
