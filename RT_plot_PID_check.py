e5e1d48b1df4512e366248c4155b5914825259f5
haakondm <74726804+haakondm@users.noreply.github.com>
2022-04-26 19:33:12 +0200

    Update RT_plot_PID_check.py

--- RT_plot_PID_check.py
+++ RT_plot_PID_check.py
@@ -8,6 +8,7 @@ import numpy as np
 import csv
 import os
 
+#---Initial values---
 sp = 0
 kp = 0
 ti = 0
@@ -24,12 +25,15 @@ root.geometry("900x600") # Window size
 
 plt.style.use('fivethirtyeight')
 
+
+#--------Save plot function--------
 def savePlot():
     plt.savefig("sacitErKjekk.png")
     
-
+#-------Plot function to animate--------
 def animate(i):
     
+    #-----Reads csv file & collecting data-----
     data = pd.read_csv('PID_temp.csv')
     x = data["x"]
     dtemp0 = data["dtemp0"]
@@ -52,19 +56,19 @@ def animate(i):
     plt.legend(loc='upper left', prop={'size':10})
     plt.tight_layout()
 
-    
+#----------------Plot window in GUI----------------
 canvas = FigureCanvasTkAgg(plt.gcf(), master=root)
 canvas.get_tk_widget().place(x = 10, y = 10, width = 600, height = 400)
 canvas.draw()
 
-
+#------------------Animate function------------------
 ani = FuncAnimation(plt.gcf(), animate, interval=500)
 
-
+#-------Setting regulator values-------
 def SetRegVals():  
     
     global sp, kp, ti, td, auto, man
-    
+    #-----Gets values from input fields-----
     sp = SP_ent.get()
     sp = float(sp)
     kp = kp_ent.get()
@@ -74,29 +78,31 @@ def SetRegVals():
     td = td_ent.get()
     td = float(td)
     
+#-----Sets auto/manual mode from checkbox-----
     if (var0.get() == 1):
         auto = 1
     else:
         auto = 0
-    
+    #-----Gets value from input field-----
     man = man_ent.get()
     man = float(man)
     
-
+#-----------Writes the regulator values to file-------------
     with open ('pid.conf', 'w') as f:
         f.write('%s,%s,%s,%s,%s,%s'%(sp,kp,ti,td,auto,man))
 
-#------Create buttons------
-
+#-------Creates checkbutton-------
 root.update()
 var0 = tk.IntVar()
 MA = tk.Checkbutton(root, text='AUTO', variable=var0, onvalue=1, offvalue=0)
 MA.place(x = 670, y = 50)
 
+#-------Creates button-------
 root.update()
 S_P = tk.Button(root, text = "Save plot", font = ('calibri', 12), command = lambda: savePlot())
 S_P.place(x = 670, y = 90)
 
+#-------Create input fields--------
 root.update()
 SP_label = tk.Label(root, text = 'SP:', font = ('calibre', 10))
 SP_label.place(x = 640, y = 140)
@@ -132,10 +138,10 @@ man_ent = tk.Entry(root)
 man_ent.insert(0, "0")
 man_ent.place(x = 670, y = td_label.winfo_y()+td_label.winfo_reqwidth() + 10)
 
+#-------Creates button-------
 root.update()
 SV = tk.Button(root, text = "SET", font = ('calibri', 12), command = lambda: SetRegVals())
 SV.place(x = 670, y = 350)
         
-    
    
 root.mainloop()
