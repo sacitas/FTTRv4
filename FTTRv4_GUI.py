9e4d5158abe0875f13d0d9448dccaa4c8b01becf
haakondm <74726804+haakondm@users.noreply.github.com>
2022-04-29 10:20:40 +0200

    Update FTTRv4_GUI.py

--- FTTRv4_GUI.py
+++ FTTRv4_GUI.py
@@ -142,9 +142,8 @@ SV = tk.Button(root, text = "SET", font = ('calibri', 12), command = lambda: Set
 SV.place(x = 900, y = 360)
 
 
-data1 = pd.read_csv('PID_temp.csv')
-dtemp0 = data1["dtemp0"]
-dtemp0 = str(dtemp0) 
+data1 = pd.read_csv('PID_temp.csv', skiprows=1)
+dtemp0 = data1[0]
 
 with open ('pid.conf', 'r+') as f:
     config = f.readline().split(',')
