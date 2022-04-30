8aa611c0dfbb9bebd65da73af5ba0ca04ee2e634
haakondm <74726804+haakondm@users.noreply.github.com>
2022-04-30 17:16:28 +0200

    Update temp_to_LCD.py

--- temp_to_LCD.py
+++ temp_to_LCD.py
@@ -1,10 +1,5 @@
-# Import LCD library
 from RPLCD import i2c
-
-# Import sleep library
 import time
-
-
 import Adafruit_ADS1x15
 
 adc = Adafruit_ADS1x15.ADS1115()
@@ -23,24 +18,33 @@ address = 0x27
 port = 1 # 0 on an older Raspberry Pi
 
 
-
 # Initialise the LCD
 lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
                   cols=cols, rows=rows)
 
 
-while True:
-    S1 = adc.read_adc(0, gain = GAIN)
-    V1 = S1*(5.0/65535)
-    temp1 = V1 / (8/1000)
-    temp1 = str(round(temp1, 1))
-    S2 = adc.read_adc(1, gain = GAIN)
-    V2 = S2*(5.0/65535)
-    temp2 = V2 / (8/1000)
-    temp2 = str(round(temp2, 1))
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string("S1 Temp: " + temp1)
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string("S2 Temp: " + temp2)
+lcd.write_string("Welcome!")
+lcd.cursor_pos = (0, 0)
+time.sleep(2)
 
-    time.sleep(0.5)
+lcd.write_string("Modes: ")
+lcd.cursor_pos = (0, 0)
+lcd.write_string("Auto/Manual")
+lcd.cursor_pos = (1, 0)
+
+
+#while True:
+#    S1 = adc.read_adc(0, gain = GAIN)
+#    V1 = S1*(5.0/65535)
+#    temp1 = V1 / (8/1000)
+#    temp1 = str(round(temp1, 1))
+#    S2 = adc.read_adc(1, gain = GAIN)
+#    V2 = S2*(5.0/65535)
+#    temp2 = V2 / (8/1000)
+#    temp2 = str(round(temp2, 1))
+#    lcd.cursor_pos = (0, 0)
+#    lcd.write_string("S1 Temp: " + temp1)
+#    lcd.cursor_pos = (1, 0)
+#    lcd.write_string("S2 Temp: " + temp2)
+
+#    time.sleep(0.5)
