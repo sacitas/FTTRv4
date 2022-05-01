f5a49e1a04ea2b034b7a2ddae88694597fad5937
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-01 12:33:29 +0200

    Update atemp_to_LCD.py

--- atemp_to_LCD.py
+++ atemp_to_LCD.py
@@ -13,7 +13,7 @@ GAIN = 1
 # constants to initialise the LCD
 lcdmode = 'i2c'
 cols = 16
-rows = 2
+rows = 4
 charmap = 'A02'
 i2c_expander = 'PCF8574'
 
@@ -27,22 +27,20 @@ port = 1 # 0 on an older Raspberry Pi
 lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
                   cols=cols, rows=rows)
 
-lcd.cursor_pos = (0, 0)
-lcd.write_string("Hello!")
 
 
-#while True:
-#    S1 = adc.read_adc(0, gain = GAIN)
-#    V1 = S1*(5.0/65535)
-#    temp1 = V1 / (7/1000)
-#    temp1 = str(round(temp1, 1))
-#    S2 = adc.read_adc(1, gain = GAIN)
-#    V2 = S2*(5.0/65535)
-#    temp2 = V2 / (7/1000)
-#    temp2 = str(round(temp2, 1))
-#    lcd.cursor_pos = (0, 0)
-#    lcd.write_string("S1 Temp: " + temp1)
-#    lcd.cursor_pos = (1, 0)
-#    lcd.write_string("S2 Temp: " + temp2)
+while True:
+    S1 = adc.read_adc(0, gain = GAIN)
+    V1 = S1*(5.0/65535)
+    temp1 = V1 / (7/1000)
+    temp1 = str(round(temp1, 1))
+    S2 = adc.read_adc(1, gain = GAIN)
+    V2 = S2*(5.0/65535)
+    temp2 = V2 / (7/1000)
+    temp2 = str(round(temp2, 1))
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string("S1 Temp: " + temp1)
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("S2 Temp: " + temp2)
 
-#    time.sleep(0.5)
+    time.sleep(0.5)
