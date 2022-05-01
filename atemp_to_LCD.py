d1a37237e81124ea95ec05a2fb2c700332410436
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-01 12:23:47 +0200

    Update atemp_to_LCD.py

--- atemp_to_LCD.py
+++ atemp_to_LCD.py
@@ -19,7 +19,7 @@ i2c_expander = 'PCF8574'
 
 # Generally 27 is the address;Find yours using: i2cdetect -y 1 
 address = 0x27 
-port = 1 # 0 on an older Raspberry Pi
+port = 0 # 0 on an older Raspberry Pi
 
 
 
