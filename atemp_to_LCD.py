a1672bc24493d2a36fd792f5d688782c3bc9eff1
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-01 11:16:34 +0200

    Update atemp_to_LCD.py

--- atemp_to_LCD.py
+++ atemp_to_LCD.py
@@ -1,11 +1,14 @@
+# Import LCD library
 from RPLCD import i2c
+
+# Import sleep library
 import time
+
 import Adafruit_ADS1x15
 
 adc = Adafruit_ADS1x15.ADS1115()
 
 GAIN = 1
-
 
 # constants to initialise the LCD
 lcdmode = 'i2c'
@@ -17,6 +20,7 @@ i2c_expander = 'PCF8574'
 # Generally 27 is the address;Find yours using: i2cdetect -y 1 
 address = 0x27 
 port = 1 # 0 on an older Raspberry Pi
+
 
 
 # Initialise the LCD
