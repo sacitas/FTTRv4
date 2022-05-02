844c4325a2cdb87296875789e29b87df2aa5865a
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-02 20:16:28 +0200

    Update FTTRv4_GUI.py

--- FTTRv4_GUI.py
+++ FTTRv4_GUI.py
@@ -72,8 +72,10 @@ def init_time_plot():
 def savePlot():
     init_time_plot()
     plt.savefig(f'{plot_filepath}.png')
-    save_label.pack_forget()
+    save_label.pack()
     time.sleep(3)
+    save_label.pack_forget()
+
     
     
 #-------Plot function to animate--------
@@ -260,7 +262,7 @@ S.place(x = 750, y = 580, width=100, heigh=30)
 
 save_label = tk.Label(root, text = 'Plot saved!', font = ('calibre', 10))
 save_label.place(x = 750, y = 610)
-save_label.pack()
+save_label.pack_forget()
 
 #-------Create input fields--------
 root.update()
