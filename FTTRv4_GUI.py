1c79e412b60900c8a8e892731bd0cc5ebcb79e64
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-02 18:42:33 +0200

    Update FTTRv4_GUI.py

--- FTTRv4_GUI.py
+++ FTTRv4_GUI.py
@@ -251,7 +251,7 @@ MA.place(x = 940, y = 30)
 #-------Creates button-------
 root.update()
 S = tk.Button(root, text = "Save plot", font = ('calibri', 12), command = lambda: savePlot())
-S.place(x = 750, y = 580)
+S.place(x = 750, y = 580, width=100, heigh=30)
 
 #-------Create input fields--------
 root.update()
