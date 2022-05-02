e9629f8bb00c42339c0b2a7a7abb340d89d4d357
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-02 14:13:51 +0200

    Update FTTRv4_GUI.py

--- FTTRv4_GUI.py
+++ FTTRv4_GUI.py
@@ -45,7 +45,7 @@ plot_filepath = ""
 root = tk.Tk()
 root.title("Real Time Plot")
 root.configure(background = 'light grey')
-root.geometry("1200x840") # Window size
+root.geometry("1250x840") # Window size
 
 plt.style.use('fivethirtyeight')
 
