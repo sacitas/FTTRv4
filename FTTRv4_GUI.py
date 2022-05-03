5f21635e151365b39bf8a0ed240c15d2f77817d1
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-03 10:50:43 +0200

    Update FTTRv4_GUI.py

--- FTTRv4_GUI.py
+++ FTTRv4_GUI.py
@@ -316,7 +316,7 @@ root.update()
 A1_label = tk.Label(root, text = 'Analog\nsensor 1: ', font = ('calibre', 10))
 A1_label.place(x = 890, y = 530)
 
-sensord0_c = tk.Label(root, text = 'Sensor d0', font = ('calibre', 10, 'bold'))
+sensord0_c = tk.Label(root, text = 'Sensor d0', font = ('calibre', 10, 'bold'), fg = '#4876FF')
 sensord0_c.place(x = 20, y = 580)
 sensord1_c = tk.Label(root, text = 'Sensor d1', font = ('calibre', 10, 'bold'), fg = '#EE0000')
 sensord1_c.place(x = 120, y = 580)
