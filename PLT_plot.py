519fc47fdcab7a47eb7b1639ce033d4627998160
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-03 12:57:31 +0200

    Update PLT_plot.py

--- PLT_plot.py
+++ PLT_plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+degree_sign = u'\N{DEGREE SIGN}'
 
 def plot(filename):
 
@@ -23,13 +24,13 @@ def plot(filename):
     plt.plot(x, temp3, label = config.sensors['sensor_3'], linewidth = 1.5, color = 'c')
     plt.plot(x, temp4, label = config.sensors['sensor_4'], linewidth = 1.5, color = 'm')
 
-    plt.xlabel('Time')
-    plt.ylabel('Temperature[C]')
+    plt.xlabel('Time [hh:mm:ss]')
+    plt.ylabel('Temperature' + '[' + degree_sign + 'C', fontsize=10)
     plt.title(str(x[0]), fontsize = 15)
-    plt.ylim([0, 200])
-    plt.xticks(rotation=45, ha='right', fontsize=12)
-    plt.xticks(np.arange(0, len(x), 30))
-    plt.legend(loc='upper left', prop={'size':12})
+    plt.ylim([0, 150])
+    plt.xticks(rotation=45, ha='right', fontsize=8)
+    plt.xticks(np.arange(0, len(x), 80))
+    plt.legend(loc='lower right', prop={'size':10})
     plt.grid()
     plt.tight_layout()
     plt.show()
