1c5835a53b9ac0b74d1a96dd2075a8731de429c1
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-03 13:11:03 +0200

    Update PLT_plot.py

--- PLT_plot.py
+++ PLT_plot.py
@@ -5,9 +5,29 @@ import pandas as pd
 
 degree_sign = u'\N{DEGREE SIGN}'
 
+plot_folder = "plot/"
+plot_filename = ""
+plot_filepath = ""
+
+
+def init_time():
+    global ref_time  
+    global plot_folder
+    global plot_filename
+    global plot_filepath
+    
+    now = dt.datetime.now()
+    t = now.strftime("%H:%M:%S")
+    (h, m, s) = t.split(':')
+    ref_time = int(h) * 3600 + int(m) * 60 + int(s)
+    
+    plot_filename = now.strftime("%m_%d_%Y-%H:%M")
+    plot_filepath = plot_folder + plot_filename
+
+
 def plot(filename):
 
-    path_to_file = '../temp/' + filename
+    path_to_file = 'temp/' + filename
 
     data = pd.read_csv(path_to_file)
     x = data["x"]
@@ -34,3 +54,5 @@ def plot(filename):
     plt.grid()
     plt.tight_layout()
     plt.show()
+    plt.savefig(f'{plot_filepath}.png')
+    
