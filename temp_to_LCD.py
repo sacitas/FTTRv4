69942141250b089eb07086e1b230368b3ed2110c
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-05 12:55:14 +0200

    Update temp_to_LCD.py

--- temp_to_LCD.py
+++ temp_to_LCD.py
@@ -123,21 +123,20 @@ def auto_mode():
 
 def man_mode():
     isPressed1 = False
-    isPressed2 = False
     global SP, Kp, Ti, Td, auto, man, ManVal
-    
-    if(GPIO.event_detected(23)):
-        if not isPressed1:
-            chan0 = AnalogIn(ads, ADS.P0)
-            V1 = chan0.voltage
-            ManVal = (V1*100.5)/3.3
-            ManVal = str(round(ManVal, 0))
-            lcd.clear()
-            lcd.cursor_pos = (0, 0)
-            lcd.write_string("ManVal: " + ManVal + "%")
-            isPressed1 = True
+    chan0 = AnalogIn(ads, ADS.P0)
+    V1 = chan0.voltage
+    ManVal = (V1*100.5)/3.3
+    ManVal = str(round(ManVal, 0))
+    temp0 = tmp.read_temp0()
+    temp0 = str(temp0)
+    lcd.clear()
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string("ManVal: " + ManVal + "%")
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("PV: " + temp0 + " " + degree_sign + "C") 
         
-    elif(GPIO.event_detected(24)):
+    if(GPIO.event_detected(24)):
         man = ManVal
         GPIO.output(27, False)
         time.sleep(0.1)
@@ -150,19 +149,9 @@ def man_mode():
         GPIO.output(27, False)
         with open ('pid.conf', 'w') as f:
             f.write('%s,%s,%s,%s,%s,%s'%(SP,Kp,Ti,Td,auto,man))
-        time.sleep(3)
-        isPressed2 = True
-    
     else:
-        readConfig()
-        temp0 = tmp.read_temp0()
-        temp0 = str(temp0)
-        man = str(man)
-        lcd.clear()
-        lcd.cursor_pos = (0, 0)
-        lcd.write_string("ManVal: " + man + "%")
-        lcd.cursor_pos = (1, 0)
-        lcd.write_string("PV: " + temp0 + " " + degree_sign + "C")   
+        pass 
+  
     
 try:
     lcd.clear()
