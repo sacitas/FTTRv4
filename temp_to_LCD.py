d8e724ddc598f5c00fd748e2286c8edbfb70cf10
haakondm <74726804+haakondm@users.noreply.github.com>
2022-05-05 10:04:17 +0200

    Update temp_to_LCD.py

--- temp_to_LCD.py
+++ temp_to_LCD.py
@@ -141,19 +141,19 @@ def man_mode():
         GPIO.output(27, True)
         time.sleep(0.1)
         GPIO.output(27, False)
+        lcd.clear()
         with open ('pid.conf', 'w') as f:
             f.write('%s,%s,%s,%s,%s,%s'%(SP,Kp,Ti,Td,auto,man))
     else:
-        pass 
-    readConfig()
-    temp0 = tmp.read_temp0()
-    temp0 = str(temp0)
-    man = str(man)
-    lcd.clear()
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string("ManVal: " + man + "%")
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string("PV: " + temp0 + " " + degree_sign + "C")  
+        readConfig()
+        temp0 = tmp.read_temp0()
+        temp0 = str(temp0)
+        man = str(man)
+        lcd.clear()
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string("ManVal: " + man + "%")
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string("PV: " + temp0 + " " + degree_sign + "C")  
 
     
 try:
